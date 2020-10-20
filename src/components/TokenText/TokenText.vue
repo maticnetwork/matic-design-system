394adb03c885cb41c7d653bea3c55a9fc7defa57
ankitboghra <ankitboghra@gmail.com>
2020-10-20 18:45:13 +0530

    token text component

---
+++ src/components/TokenText/TokenText.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="token-text">
+    <Textfield
+      type="number"
+      nature="link"
+      :placeholder="placeholder"
+      :size="size"
+    />
+    <Button
+      backgroundColor="#F3F4F7"
+      :class="buttonClasses"
+      :iconName="currencySymbol"
+      iconNameRight="discloser/bottom"
+      :label="currencyName"
+      size="medium"
+    />
+  </div>
+</template>
+
+<script>
+import "./tokenText.scss";
+import Textfield from "../Textfield/Textfield";
+import Button from "../Button/Button";
+
+export default {
+  name: "TokenText",
+
+  components: {
+    Textfield,
+    Button,
+  },
+
+  props: {
+    size: {
+      type: String,
+      default: "medium",
+      validator: function (value) {
+        return ["medium", "large"].indexOf(value) !== -1;
+      },
+    },
+    currencySymbol: {
+      type: String,
+    },
+    currencyName: {
+      type: String,
+    },
+    placeholder: {
+      type: String,
+      default: "0.00",
+    },
+  },
+
+  computed: {
+    buttonClasses() {
+      return {
+        "select-box": true,
+        "select-box--large": this.size == "large",
+        "select-box--medium": this.size == "medium",
+      };
+    },
+    buttonSize() {
+      return this.size === large ? "medium" : "small";
+    },
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Button from "./components/Button/Button";
 import Icon from "./components/Icon/Icon";
 import Textfield from "./components/Textfield/Textfield";
+import TokenText from "./components/TokenText/TokenText";
 
 export {
     Button,
     Icon,
-    Textfield
+    Textfield,
+    TokenText,
 }
