0965f3efcece5b8356dac0516b2382ccf66e7f2a
ankitboghra <ankitboghra@gmail.com>
2021-01-26 16:19:05 +0530

    rmv: remove header export

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 // form components
 export { default as Button } from "./components/Button/Button";
-export { default as Header } from "./components/Header/Header";
 export { default as Textfield } from "./components/Textfield/Textfield";
 export { default as TokenText } from "./components/TokenText/TokenText";
 
