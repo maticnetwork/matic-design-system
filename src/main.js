cecb2501e70f0936191758f956e581782a076927
Salil Naik <salil.naik27@gmail.com>
2021-08-04 18:20:31 +0530

    add: basic button component

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 // form components
-export { default as Button } from "./components/Button/Button";
-export { default as ButtonLoader } from "./components/Button/ButtonLoader";
-export { default as Textfield } from "./components/Textfield/Textfield";
-export { default as TokenText } from "./components/TokenText/TokenText";
+export { default as PButton } from "./components/Button/Button";
+// export { default as ButtonLoader } from "./components/Button/ButtonLoader";
+// export { default as Textfield } from "./components/Textfield/Textfield";
+// export { default as TokenText } from "./components/TokenText/TokenText";
 
-// components
-export { default as Icon } from "./components/Icon/Icon";
-export { default as MaticLogo } from "./components/MaticLogo/MaticLogo";
+// // components
+// export { default as Icon } from "./components/Icon/Icon";
+// export { default as MaticLogo } from "./components/MaticLogo/MaticLogo";
 
-export { default as ProgressBar } from "./components/ProgressBar/ProgressBar";
-export { default as Progress } from "./components/ProgressBar/Progress";
+// export { default as ProgressBar } from "./components/ProgressBar/ProgressBar";
+// export { default as Progress } from "./components/ProgressBar/Progress";
